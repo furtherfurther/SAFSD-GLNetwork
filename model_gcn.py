94dde941dc530ca6607b42e47cea4c6a970b5544
lhb <91206041+furtherfurther@users.noreply.github.com>
2025-07-25 12:26:37 +0800

    Update model_gcn.py

--- model_gcn.py
+++ model_gcn.py
@@ -61,14 +61,7 @@ class GraphConvolution(nn.Module):
         theta = math.log(lamda / l + 1)
         # Compute weighted features from neighbor nodes (core of convolution)
         hi = torch.spmm(adj, input)  # Sparse matrix multiplication with adjacency matrix
-        # Variant mode: concatenate current node features and initial features
-        if self.variant:
-            support = torch.cat([hi, h0], 1)  # Concatenate neighbor features and initial features
-            r = (1 - alpha) * hi + alpha * h0  # Mix neighbor info and original features
-        else:
-            support = (1 - alpha) * hi + alpha * h0  # Mix neighbor info and original features
-            r = support  # Without variant, support equals r
-        
+     
         return output
 
 
@@ -83,18 +76,6 @@ class PositionalEncoding(nn.Module):
         pe[:, 0, 1::2] = torch.cos(position * div_term)
         self.register_buffer('pe', pe)
 
-    def forward(self, x, dia_len):
-        """
-        x: Tensor, shape [seq_len, batch_size, embedding_dim]
-        """
-        tmpx = torch.zeros(0).cuda()
-        tmp = 0
-        for i in dia_len:
-            a = x[tmp:tmp+i].unsqueeze(1)
-            a = a + self.pe[:a.size(0)]
-            tmpx = torch.cat([tmpx, a], dim=0)
-            tmp = tmp + i
-        return self.dropout(tmpx)
 
 
 class GCN(nn.Module):
@@ -121,17 +102,7 @@ class GCN(nn.Module):
         # Fully connected layer in network
         self.fc1 = nn.Linear(n_dim, nhidden)  # Mapping from input dimension to hidden layer
 
-        # Graph convolution parameters
-        self.num_L = num_L
-        self.num_K = num_K
-        self.act_fn = nn.ReLU()
-        self.hyperedge_weight = nn.Parameter(torch.ones(1000))  # Hyperedge weights
-        self.EW_weight = nn.Parameter(torch.ones(5200))
-        self.hyperedge_attr1 = nn.Parameter(torch.rand(nhidden))  # Hyperedge feature 1
-        self.hyperedge_attr2 = nn.Parameter(torch.rand(nhidden))  # Hyperedge feature 2
-        # Graph convolution layers
-        for kk in range(num_K):
-            setattr(self, 'conv%d' % (kk + 1), GraphGCN(nhidden, nhidden))
+
 
     def forward(self, a, v, l, dia_len, qmask, epoch):
         qmask = torch.cat([qmask[:x, i, :] for i, x in enumerate(dia_len)], dim=0)
@@ -168,59 +139,5 @@ class GCN(nn.Module):
 
         return out1
 
-    def reverse_features(self, dia_len, features):
-        # Split features according to dialogue lengths and reorder
-        l = []
-        a = []
-        v = []
-        for i in dia_len:
-            ll = features[0:1 * i]
-            aa = features[1 * i:2 * i]
-            vv = features[2 * i:3 * i]
-            features = features[3 * i:]
-            l.append(ll)
-            a.append(aa)
-            v.append(vv)
-        tmpl = torch.cat(l, dim=0)
-        tmpa = torch.cat(a, dim=0)
-        tmpv = torch.cat(v, dim=0)
-        features = torch.cat([tmpl, tmpa, tmpv], dim=-1)
-        return features
 
-    def create_gnn_index(self, a, v, l, dia_len, modals):
-        # Create adjacency matrix (edge indices) needed for graph convolution
-        num_modality = len(modals)
-        node_count = 0
-        index = []
-        tmp = []
-        
-        for i in dia_len:
-            nodes = list(range(i * num_modality))  # Create nodes for each modality
-            nodes = [j + node_count for j in nodes]
-            nodes_l = nodes[0:i * num_modality // 3]
-            nodes_a = nodes[i * num_modality // 3:i * num_modality * 2 // 3]
-            nodes_v = nodes[i * num_modality * 2 // 3:]
-            index = index + list(permutations(nodes_l, 2)) + list(permutations(nodes_a, 2)) + list(permutations(nodes_v, 2))
-            # Create hyperedges connecting different modality nodes
-            Gnodes = []
-            for _ in range(i):
-                Gnodes.append([nodes_l[_]] + [nodes_a[_]] + [nodes_v[_]])
-            for ii, _ in enumerate(Gnodes):
-                tmp = tmp + list(permutations(_, 2))
-            # Concatenate features
-            if node_count == 0:
-                ll = l[0:0 + i]
-                aa = a[0:0 + i]
-                vv = v[0:0 + i]
-                features = torch.cat([ll, aa, vv], dim=0)
-                temp = 0 + i
-            else:
-                ll = l[temp:temp + i]
-                aa = a[temp:temp + i]
-                vv = v[temp:temp + i]
-                features_temp = torch.cat([ll, aa, vv], dim=0)
-                features = torch.cat([features, features_temp], dim=0)
-                temp = temp + i
-            node_count = node_count + i * num_modality
-        edge_index = torch.cat([torch.LongTensor(index).T, torch.LongTensor(tmp).T], 1).to("cuda:0")
-        return edge_index, features
+
