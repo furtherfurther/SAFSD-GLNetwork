8fc32731b5d250ba83ca04f01f3cf58b68f8db6b
lhb <91206041+furtherfurther@users.noreply.github.com>
2025-07-25 12:38:06 +0800

    Update train.py

--- train.py
+++ train.py
@@ -54,100 +54,6 @@ def get_IEMOCAP_loaders(batch_size=32, valid=0.1, num_workers=0, pin_memory=Fals
 
 
 
-def train_or_eval_model(model, loss_function, kl_loss, dataloader, epoch, optimizer=None, train=False, gamma_1=1.0, gamma_2=1.0, gamma_3=1.0):
-    """
-    Train or evaluate a model depending on the `train` flag.
-    Handles data loading, forward pass, loss calculation, backward pass (if training), and performance evaluation.
-    
-    :param kl_loss: Function to compute KL divergence loss.
-    :param train: Boolean flag indicating whether to train or evaluate the model.
-    :param gamma_1: Weight coefficient for the first loss term.
-    :param gamma_2: Weight coefficient for the second loss term.
-    :param gamma_3: Weight coefficient for the KL divergence loss term.
-    """
-    losses, preds, labels, masks = [], [], [], []
-    labels_g = []
-    # Ensure optimizer is provided if training
-    assert not train or optimizer is not None
-    if train:
-        model.train()
-    else:
-        model.eval()
-
-    # Iterate over batches in dataloader
-    for data in dataloader:
-        # Zero gradients if training
-        if train:
-            optimizer.zero_grad()
-        # Move data to device (e.g., CUDA) if available, prepare inputs
-        textf, visuf, acouf, qmask, umask, label = [d.cuda() for d in data[:-1]] if cuda else data[:-1]
-        
-        lengths = [(umask[j] == 1).nonzero().tolist()[-1][0] + 1 for j in range(len(umask))]
-
-        # Forward pass: get log probabilities and probabilities for different modalities and their KL counterparts
-        log_prob1, log_prob2, log_prob3, all_log_prob, all_prob, \
-        kl_log_prob1, kl_log_prob2, kl_log_prob3, kl_all_prob = model(textf, visuf, acouf, umask, qmask, lengths)
-
-        # Reshape log probabilities and labels for loss calculation
-        lp_1 = log_prob1.view(-1, log_prob1.size()[2])
-        lp_2 = log_prob2.view(-1, log_prob2.size()[2])
-        lp_3 = log_prob3.view(-1, log_prob3.size()[2])
-        lp_all = all_log_prob.view(-1, all_log_prob.size()[1])
-        labels_ = label.view(-1)
-
-        kl_lp_1 = kl_log_prob1.view(-1, kl_log_prob1.size()[1])
-        kl_lp_2 = kl_log_prob2.view(-1, kl_log_prob2.size()[1])
-        kl_lp_3 = kl_log_prob3.view(-1, kl_log_prob3.size()[1])
-        kl_p_all = kl_all_prob.view(-1, kl_all_prob.size()[1])
-
-       
-
-        # Instantiate loss functions; move to device if CUDA is used
-        loss_function_1 = nn.NLLLoss(loss_weights.to(torch.device("cuda:0")) if cuda else loss_weights)  # For IEMOCAP
-        loss_function_g = SimilarityAwareFocalLoss()  # For MELD
-        loss_function_3 = SimilarityAwareFocalLoss_2()  # (Defined but unused here)
-
-        label_g = torch.cat([label[j][:lengths[j]] for j in range(len(label))])
-
-        # Compute total loss as weighted sum of different losses
-        loss = gamma_1 * loss_function_1(all_log_prob, label_g) + \
-               gamma_2 * (loss_function_g(lp_1, labels_) + loss_function_g(lp_2, labels_) + loss_function_g(lp_3, labels_)) + \
-               gamma_3 * (kl_loss(kl_lp_1, kl_p_all) + kl_loss(kl_lp_2, kl_p_all) + kl_loss(kl_lp_3, kl_p_all))
-
-     
-
-        # Append results for evaluation
-        preds.append(pred_.data.cpu().numpy())
-        labels.append(labels_.data.cpu().numpy())
-        labels_g.append(label_g.data.cpu().numpy())
-        masks.append(umask.view(-1).cpu().numpy())
-        losses.append(loss.item() * masks[-1].sum())
-
-        # Backpropagation and optimizer step if training
-        if train:
-            loss.backward()
-            if args.tensorboard:
-                for param in model.named_parameters():
-                    writer.add_histogram(param[0], param[1].grad, epoch)
-            optimizer.step()
-
-    if preds != []:
-        preds = np.concatenate(preds)
-        labels = np.concatenate(labels)
-        labels_g = np.concatenate(labels_g)
-        masks = np.concatenate(masks)
-    else:
-        return float('nan'), float('nan'), [], [], [], float('nan')
-
-    # Calculate average loss, accuracy, and weighted F1 score
-    avg_loss = round(np.sum(losses) / np.sum(masks), 4)
-    avg_accuracy = round(accuracy_score(labels_g, preds) * 100, 2)
-    avg_fscore = round(f1_score(labels_g, preds, average='weighted') * 100, 2)
-    
-    # Return average loss, accuracy, labels, predictions, masks, and F1 score
-    return avg_loss, avg_accuracy, labels_g, preds, masks, avg_fscore
-
-
 if __name__ == '__main__':
     # Handle command line arguments
     parser = argparse.ArgumentParser()
@@ -198,19 +104,7 @@ if __name__ == '__main__':
     # Print temperature parameter
     print('temp {}'.format(args.temp))
 
-    # 2. Preparation stage for training and evaluation, ensuring model, loss functions, optimizer, and data loaders are set up correctly.
-    model = Transformer_Based_Model(args.Dataset, args.temp, D_text, D_visual, D_audio, args.n_head,
-                                        n_classes=n_classes,
-                                        hidden_dim=args.hidden_dim,
-                                        n_speakers=n_speakers,
-                                        dropout=args.dropout)
-
-    total_params = sum(p.numel() for p in model.parameters())
-    print('total parameters: {}'.format(total_params))
-    # Calculate number of trainable parameters in the model
-    total_trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-    print('training parameters: {}'.format(total_trainable_params))
-
+   
     if cuda:
         # Move model to GPU
         model.cuda()
@@ -226,12 +120,7 @@ if __name__ == '__main__':
                                                                     batch_size=batch_size,
                                                                     num_workers=0)
     elif args.Dataset == 'IEMOCAP':
-        loss_weights = torch.FloatTensor([1/0.086747,
-                                        1/0.144406,
-                                        1/0.227883,
-                                        1/0.160585,
-                                        1/0.127711,
-                                        1/0.252668])
+      
         loss_function = MaskedNLLLoss(loss_weights.cuda() if cuda else loss_weights)
         train_loader, valid_loader, test_loader = get_IEMOCAP_loaders(valid=0.001, batch_size=batch_size,
                                                                       num_workers=0)
